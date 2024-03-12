5393b78739be289e20ff0857f21cf9c641215bf9
taaha <taaha.s.bajwa@gmail.com>
2024-03-12 14:38:10 +0500

    added list instead of df

--- chat_with_document.py
+++ chat_with_document.py
@@ -12,29 +12,25 @@ st.sidebar.success("Select a demo above.")
 
 df = pd.read_csv('metadata_doc4_gpt003s_0.csv')
 
-# Show the dataframe (we'll delete this later)
-# st.write(df)
+df_no_duplicates = df.drop_duplicates(subset=['pdf_name'])
 
 # Use a text_input to get the keywords to filter the dataframe
 text_search = st.text_input("Search Casefiles", value="")
 
-# Filter the dataframe using masks
-m1 = df["legible_name"].str.contains(text_search)
-df_search = df[m1]
+names_list = df_no_duplicates['legible_name'].tolist()
 
-# Show the results, if you have a text_search
-# if text_search:
-#     st.write(df_search)
+# Filter the list based on the search text
+filtered_names = [name for name in names_list if text_search.lower() in name.lower()]
 
-# Another way to show the filtered results
-# Show the cards
-N_cards_per_row = 3
 if text_search:
-    for n_row, row in df_search.reset_index().iterrows():
-        i = n_row%N_cards_per_row
-        if i==0:
-            st.write("---")
-            cols = st.columns(N_cards_per_row, gap="large")
-        # draw the card
-        with cols[n_row%N_cards_per_row]:
-            st.caption(f"{row['legible_name'].strip()}")
+    # Show the cards
+    N_cards_per_row = 3
+    if text_search:
+        for n_row, row in enumerate(filtered_names):
+            i = n_row%N_cards_per_row
+            if i==0:
+                st.write("---")
+                cols = st.columns(N_cards_per_row, gap="large")
+            # draw the card
+            with cols[n_row%N_cards_per_row]:
+                st.caption(f"{row}")
